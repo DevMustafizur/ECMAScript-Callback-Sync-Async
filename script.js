c4f8744d35cd994e6a63915ead18a2936127c37e
Mustafizur <mustafizurrahman1256@gmail.com>
2026-08-25 10:18:13 +0600

    Add callback sync and async examples

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+// syncronus
+class myError extends Error {
+    constructor(message, statusCode) {
+        super(message)
+        this.statusCode = statusCode;
+    }
+}
+
+
 const products = [
     {
         productId: 101,
@@ -14,132 +23,320 @@ const products = [
 ];
 
 
-function Cart(userId) {
+function myCart(userId) {
     this.userId = userId;
-    this.items = [];
+    this.carts = [];
 }
 
-
-Cart.prototype.findProduct = function (productId, callback) {
+myCart.prototype.findProduct = function (productId, callback) {
+    let findProduct = null;
     for (let i = 0; i < products.length; i++) {
         if (products[i].productId === productId) {
-            return callback(null, products[i])
+            findProduct = products[i];
+            break;
         }
-        return callback(new Error("Product Not Found", null))
     }
-};
+    if (findProduct) {
+        return callback(null, findProduct)
+    }
+    return callback(new myError("Product Not Found", 404), null)
+}
 
+myCart.prototype.checkStock = function (productId, quantity, callback) {
+    this.findProduct(productId, (error, data) => {
+        if (error) {
+            return callback(error)
+        }
+        if (data.stock < quantity) {
+            return callback(new myError("Stock insufficient", 404), null)
+        }
+        return callback(null, data)
+    })
+}
 
-Cart.prototype.checkStock = function (productId, quantity, callback) {
-    this.findProduct(productId, (error, product) => {
+myCart.prototype.findCartItem = function (productId, callback) {
+    let findProduct = null
+    for (let i = 0; i < this.carts.length; i++) {
+        if (this.carts[i]?.productId === productId) {
+            findProduct = this.carts[i]
+            break
+        }
+    }
+    if (findProduct) {
+        return callback(new myError("Product already added to cart", 404), null)
+    }
+    return callback(null, "Product not found, you can add this product to cart")
+}
+
+myCart.prototype.calculatePrice = function (productId, quantity, callback) {
+    this.findProduct(productId, (error, data) => {
         if (error) {
             return callback(error, null)
         }
-        if (product.stock < quantity) {
-            return callback(
-                new Error("Insufficient stock"),
-                null
-            );
-        }
-        return callback(null, product)
-    })
-};
-
-
-Cart.prototype.checkCartItem = function (productId, callback) {
-    for (let i = 0; i < this.items.length; i++) {
-        if (this.items[i].productId === productId) {
-            return callback(null, products[i])
-        }
-    }
-    return callback(null, null)
-};
-
-
-Cart.prototype.calculatePrice = function (
-    productId,
-    quantity,
-    callback
-) {
-    this.findProduct(productId, (error, product)=>{
-        if(error){
-           return callback(error, null)
-        }
-        const totalPrice = product.price * quantity;
-
+        const totalPrice = data.price * quantity;
         return callback(null, totalPrice)
     })
-};
+}
 
+myCart.prototype.saveCartItem = function (productId, quantity, totalPrice, callback) {
+    const storeCart = {
+        productId,
+        quantity,
+        totalPrice,
+    }
 
-Cart.prototype.save = function (cart, callback) {
-    this.items.push(cart)
-    return callback(null, cart)
-};
+    // setTimeout(() => {
+    //     const result = this.carts.push(storeCart)
+    //     if (!result) {
+    //         return callback(new myError("something went wrong", 404), null)
+    //     }
+    //     return callback(null, { success: true, message: "cart added", data: storeCart })
+    // }, 2000)
 
+    // sync code
+    const result = this.carts.push(storeCart)
+    if (!result) {
+        return callback(new myError("something went wrong", 404), null)
+    }
+    return callback(null, { success: true, message: "cart added", data: storeCart })
+}
 
-Cart.prototype.addItem = function (
-    productId,
-    quantity,
-    callback
-) {
-
-    this.findProduct(productId, (error, product) => {
+myCart.prototype.addToCart = function (productId, quantity, callback) {
+    this.findProduct(productId, (error, data) => {
         if (error) {
-           return callback(error, null)
+            return callback(error, null)
         }
-
-        this.checkStock(productId, quantity, (error, product) => {
+        this.checkStock(productId, quantity, (error, data) => {
             if (error) {
                 return callback(error, null)
             }
-            this.checkCartItem(productId, (error, cartItem) => {
+            this.findCartItem(productId, (error, data) => {
                 if (error) {
                     return callback(error, null)
                 }
-                if (cartItem) {
-                    return callback(new Error("Product Already Added", null))
-                }
-
-                this.calculatePrice(productId, quantity, (error, totalPrice)=>{
-                    if(error){
-                        return callback(error, null)
+                this.calculatePrice(productId, quantity, (error, totalPrice) => {
+                    if (error) {
+                        return callback(error)
                     }
-                    const cart = {
-                        productId,
-                        quantity,
-                        totalPrice
-                    }
-                    this.save(cart, (error, data)=>{
-                        if(error){
-                            callback(error, null)
+                    this.saveCartItem(productId, quantity, totalPrice, (error, info) => {
+                        if (error) {
+                            return callback(error, null)
                         }
-                        return callback(null, data)
+                        return callback(null, info)
                     })
                 })
-
             })
         })
     })
-};
+}
 
+const user1 = new myCart("10001")
 
-
-const myCart = new Cart(101);
-myCart.addItem(101, 19, (error, data) => {
-    if(error){
-        console.log(error)
+// first added
+user1.addToCart(101, 4, (error, data) => {
+    if (error) {
+        console.log(error.message)
     }
-    console.log(data)
-});
-
-myCart.addItem(101, 19, (error, data) => {
-    if(error){
-        return console.log(error.message)
-    } 
-    console.log(data)
-});
+    if (data) {
+        console.log(data)
+    }
+})
 
 
-console.log(myCart.items)
+// second added
+user1.addToCart(101, 5, (error, data) => {
+    if (error) {
+        console.log(error.message)
+    }
+    if (data) {
+        console.log(data)
+    }
+})
 
+// third added
+user1.addToCart(101, 2, (error, data) => {
+    if (error) {
+        console.log(error.message)
+    }
+    if (data) {
+        console.log(data)
+    }
+})
+
+
+// fourth added
+user1.addToCart(101, 1, (error, data) => {
+    if (error) {
+        console.log(error.message)
+    }
+    if (data) {
+        console.log(data)
+    }
+})
+
+console.log('this is a work')
+
+
+// // asyncronous
+// class myError extends Error {
+//     constructor(message, statusCode) {
+//         super(message)
+//         this.statusCode = statusCode;
+//     }
+// }
+
+
+// const products = [
+//     {
+//         productId: 101,
+//         name: "T-Shirt",
+//         price: 500,
+//         stock: 20
+//     },
+//     {
+//         productId: 102,
+//         name: "Shoes",
+//         price: 1500,
+//         stock: 10
+//     }
+// ];
+
+
+// function myCart(userId) {
+//     this.userId = userId;
+//     this.carts = [];
+// }
+
+// myCart.prototype.findProduct = function (productId, callback) {
+//     let findProduct = null;
+//     for (let i = 0; i < products.length; i++) {
+//         if (products[i].productId === productId) {
+//             findProduct = products[i];
+//             break;
+//         }
+//     }
+//     if (findProduct) {
+//         return callback(null, findProduct)
+//     }
+//     return callback(new myError("Product Not Found", 404), null)
+// }
+
+// myCart.prototype.checkStock = function (productId, quantity, callback) {
+//     this.findProduct(productId, (error, data) => {
+//         if (error) {
+//             return callback(error)
+//         }
+//         if (data.stock < quantity) {
+//             return callback(new myError("Stock insufficient", 404), null)
+//         }
+//         return callback(null, data)
+//     })
+// }
+
+// myCart.prototype.findCartItem = function (productId, callback) {
+//     let findProduct = null
+//     for (let i = 0; i < this.carts.length; i++) {
+//         if (this.carts[i]?.productId === productId) {
+//             findProduct = this.carts[i]
+//             break
+//         }
+//     }
+//     if (findProduct) {
+//         return callback(new myError("Product already added to cart", 404), null)
+//     }
+//     return callback(null, "Product not found, you can add this product to cart")
+// }
+
+// myCart.prototype.calculatePrice = function (productId, quantity, callback) {
+//     this.findProduct(productId, (error, data) => {
+//         if (error) {
+//             return callback(error, null)
+//         }
+//         const totalPrice = data.price * quantity;
+//         return callback(null, totalPrice)
+//     })
+// }
+
+// myCart.prototype.saveCartItem = function (productId, quantity, totalPrice, callback) {
+//     const storeCart = {
+//         productId,
+//         quantity,
+//         totalPrice,
+//     }
+
+//     setTimeout(() => {
+//         const result = this.carts.push(storeCart)
+//         if (!result) {
+//             return callback(new myError("something went wrong", 404), null)
+//         }
+//         return callback(null, { success: true, message: "cart added", data: storeCart })
+//     }, 2000)
+
+// }
+
+// myCart.prototype.addToCart = function (productId, quantity, callback) {
+//     this.findProduct(productId, (error, data) => {
+//         if (error) {
+//             return callback(error, null)
+//         }
+//         this.checkStock(productId, quantity, (error, data) => {
+//             if (error) {
+//                 return callback(error, null)
+//             }
+//             this.findCartItem(productId, (error, data) => {
+//                 if (error) {
+//                     return callback(error, null)
+//                 }
+//                 this.calculatePrice(productId, quantity, (error, totalPrice) => {
+//                     if (error) {
+//                         return callback(error)
+//                     }
+//                     this.saveCartItem(productId, quantity, totalPrice, (error, info) => {
+//                         if (error) {
+//                             return callback(error, null)
+//                         }
+//                         return callback(null, info)
+//                     })
+//                 })
+//             })
+//         })
+//     })
+// }
+
+// const user1 = new myCart("10001")
+
+// // first added
+// user1.addToCart(101, 4, (error, data) => {
+//     if (error) {
+//         console.log(error.message)
+//     }
+//     console.log(data)
+// })
+
+
+// // second added
+// user1.addToCart(101, 5, (error, data) => {
+//     if (error) {
+//         console.log(error.message)
+//     }
+//     console.log(data)
+// })
+
+// // third added
+// user1.addToCart(101, 2, (error, data) => {
+//     if (error) {
+//         console.log(error.message)
+//     }
+//     console.log(data)
+// })
+
+
+// // fourth added
+// user1.addToCart(101, 1, (error, data) => {
+//     if (error) {
+//         console.log(error.message)
+//     }
+//     console.log(data)
+// })
+
+// console.log('this is a work')
